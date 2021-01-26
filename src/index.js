91586764dbe74a302994c445c170c27d81c62432
Dmitry Grinchenko <ra1d3r@i.ua>
2021-01-26 16:12:43 +0200

    added countries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+import { template } from "handlebars";
 import "./css/style.css";
+import countries from "./data-base/countries.json";
+import countriesTemplate from "./templates/countries.hbs";
+// console.log(countries);
+console.log(countriesTemplate(countries));
+const markup = countriesTemplate(countries);
+document.querySelector(".gallery").insertAdjacentHTML("beforeend", markup);
+
+// ===========================Pockemon============================================
+
+// import ditto from "./data-base/ditto.json";
+// import bulbasaur from "./data-base/bulbasaur.json";
+// import pokemonTemplate from "./templates/pokemon.hbs";
+
+// console.log(ditto);
+// console.log(pokemonTemplate(ditto));
+
+// const markup = pokemonTemplate(ditto);
+// const markup2 = pokemonTemplate(bulbasaur);
+
+// document.body.insertAdjacentHTML("beforeend", markup);
+// document.body.insertAdjacentHTML("beforeend", markup2);
+
+// ===================================================================================
